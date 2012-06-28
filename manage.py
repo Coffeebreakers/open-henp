59afe2d1f9ca7e69e769d78008743d6fa0ff5b00
jlazarini <jlazarini@REC-368617.(none)>
2012-06-28 10:28:31 -0300

    Corrigido erro do python para a geracao do xpi do henp

--- manage.py
+++ manage.py
@@ -2,15 +2,37 @@
 import zipfile
 import os
 
+class ZipUtilities:
 
+  def toZip(self, directory, xpiname):
+
+      xpicreate = zipfile.ZipFile(xpiname, 'w', zipfile.ZIP_DEFLATED)
+      for dirpath, dirnames, filenames in os.walk(directory):
+          for filename in filenames:
+              full_path = os.path.join(dirpath, filename)
+              file_path = full_path[len(directory)+1:]
+              print 'File added: ' + str(file_path)
+              xpicreate.write(full_path, file_path)
+      print 'File "' + xpiname + '" successfully created'
+
+def main():
+    utilities = ZipUtilities()
+    xpiname = 'open-henp.xpi'
+    directory = 'src'
+    utilities.toZip(directory, xpiname)
+
+main()
+
+'''
 class ZipUtilities:
 
     def toZip(self, file, filename):
+        print pavement_file
         zip_file = zipfile.ZipFile(filename, 'w')
         if os.path.isfile(file):
-        	zip_file.write(file)
-        else:
-        	self.addFolderToZip(zip_file, file)
+            zip_file.write(file)
+       # else:
+        #   self.addFolderToZip(zip_file, file)
         zip_file.close()
 
     # TODO: Remove the src folder path during the zip generation
@@ -27,8 +49,9 @@ class ZipUtilities:
 def main():
     utilities = ZipUtilities()
     filename = 'open-henp.zip'
-    directory = 'src'
+    directory = 'src/install.rdf'
     utilities.toZip(directory, filename)
     os.rename(filename, 'open-henp.xpi')
 
 main()
+'''
