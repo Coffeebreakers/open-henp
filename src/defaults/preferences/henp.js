a62a0eef65628df79be6cb69da9ccb136ac70c2c
Mario Felipe Rinaldi <lalo.rinaldi@gmail.com>
2012-09-25 10:06:17 -0300

    Arrumando a casa, generalizando as coisas para funcionar com 40h ou 44h por semana, e 5 ou 6 dias por semana.

--- src/defaults/preferences/henp.js
+++ src/defaults/preferences/henp.js
@@ -2,9 +2,10 @@ pref("henp.enabled", false);
 pref("henp.alertIndex", 100);
 
 pref("henp.initialTime", '09:00');
+pref("henp.limitTime", '00:00');
+pref("henp.hoursPerWeek", '40:00');
+pref("henp.daysPerWeek", '5');
 pref("henp.targetTime", '0');
-pref("henp.workHours", 0);
-pref("henp.holidaySaturday", false);
 pref("henp.alertMethodSound", true);
 pref("henp.alertSoundFile", 'default');
 
